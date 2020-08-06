6bf2a40e8b9637e615bb08065116d65f321409c6
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-08-06 18:05:16 -0400

    met MVP!

--- src/DisplayArcadeGame.js
+++ src/DisplayArcadeGame.js
@@ -6,17 +6,33 @@ import { useParams } from "react-router-dom";
 export default function ArcadeGame(props) {
   const params = useParams()
 
-  const rhythmgame = props.rhythmGames.find((r) => r.fields.Locations.includes(props.arcadeId) );
-  if (rhythmgame === undefined) return "No Rhythm Games here :( "
-  // const rhythmgame = props.rhythmGames.find((r) => r.fields.Images === params.Images);
+  console.log(props.arcadeId)
+  if (!props.rhythmGames.length) return null
+  // console.log(props.rhythmGames[0].fields['Table 1'].includes(1))
+  const matchingRhythmGames = props.rhythmGames.filter((s) => {
+    console.log(s)
+    if (!s.fields.hasOwnProperty('Table 1')) return false
+    return s.fields['Table 1'].includes(props.arcadeId)
+  })
 
+  if (matchingRhythmGames === undefined) return 'No rhythm games here'
   return (
     <div className='pic-container'>
-      {/* {rhythmGames.map((rhythmgames) => (
-        <img className='gameMachine' src={rhythmgames.fields.Images}/>
-      ))} */}
-    <img className="gameMachines" src={rhythmgame.fields.Images}/>
-      {/* console.log(rhythmgame.fileds.image) */}
+      {matchingRhythmGames.map((m) => <img className="gameMachines" src={m.fields.Images}/> )}
     </div>
   )
+
+  // const rhythmgame = props.rhythmGames.find((r) => r.fields.Locations.includes(props.arcadeId) );
+  // if (rhythmgame === undefined) return "No Rhythm Games here :( "
+  // // const rhythmgame = props.rhythmGames.find((r) => r.fields.Images === params.Images);
+
+  // return (
+  //   <div className='pic-container'>
+  //     {/* {rhythmGames.map((rhythmgames) => (
+  //       <img className='gameMachine' src={rhythmgames.fields.Images}/>
+  //     ))} */}
+  //   <img className="gameMachines" src={rhythmgame.fields.Images}/>
+  //     {/* console.log(rhythmgame.fileds.image) */}
+  //   </div>
+  // )
 }
