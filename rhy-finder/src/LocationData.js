6a10a263b2b969e0859fca1530a928fd44fc80cb
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-08-06 16:50:02 -0400

    made public

--- rhy-finder/src/LocationData.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getArcades = async((lat = 40.730610, long = -73.935242) => {
-  const response = await axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=-${long}&term=arcade`,
-    {
-    headers: `Bearer ${process.env.REACT_APP_YELP_KEY}`,
-    })
-  return response.data.businesses
-})
